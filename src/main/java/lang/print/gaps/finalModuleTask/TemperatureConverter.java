34e255ba1b4aeaae1a46ee89eff6aff12f36e996
Max Ageev <ageev.makson@yandex.ru>
2022-07-12 12:49:55 +0300

    add formula

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        double fahrenheit = ((temperatureCelsius * 9) / 5 ) + 32;
+        double fahrenheit =  (((double)temperatureCelsius * 9) / 5 ) + 32;
         System.out.println(fahrenheit);
     }
 }
