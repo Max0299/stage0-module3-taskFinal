e16e0a00ac742f6df699cfae0824853bfd4ec9a8
Max Ageev <ageev.makson@yandex.ru>
2022-07-12 11:47:02 +0300

    fix const 33.8

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        double fahrenheit = temperatureCelsius * 38.8;
+        double fahrenheit = temperatureCelsius * 33.8;
         System.out.println(fahrenheit);
     }
 }
