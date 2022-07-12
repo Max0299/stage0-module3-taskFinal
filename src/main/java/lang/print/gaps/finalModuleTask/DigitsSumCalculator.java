1d991516f3cdc4cb9c5c8586b32b0257348dd8a8
Max Ageev <ageev.makson@yandex.ru>
2022-07-12 11:37:16 +0300

    check

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int summ = 0;
+        while(number != 0){
+            summ += number % 10;
+            number = number / 10;
+        }
+        System.out.println(summ);
     }
 }
+
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        while (number !=0){
+            System.out.print(number%10);
+            number = number /10;
+        }
+
+
     }
 }
